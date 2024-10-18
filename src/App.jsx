1adbe5ff9726a3089cb1da836a1e506845bdd6a1
KarimKashkoush <karimkashkoish5@gmail.com>
2024-10-18 18:54:32 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import JoinTheTeam from "./pages/joinTheTeam/JoinTheTeam";
-
+import Loading from "./pages/loading/Loading"; 
 function App() {
+  const [loading, setLoading] = useState(true); 
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 2000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/joinTheTeam" element={<JoinTheTeam />} />
-      </Routes>
+      {loading ? ( 
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/joinTheTeam" element={<JoinTheTeam />} />
+          </Routes>
+        </>
+      )}
     </BrowserRouter>
   );
 }
---
+++ src/pages/loading/Loading.jsx
@@ -0,0 +1,8 @@
+import './loading.css'
+export default function Loading() {
+      return (
+            <section className='loading'>
+                  <span></span>
+            </section>
+      )
+}
